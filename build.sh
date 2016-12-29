f32e3b026c5271d49c42f472695ca2e27617c464
Elliot Hawkins <elliotshawkins@gmail.com>
2016-12-28 20:33:44 -0800

    Add SDL2 Mixer linking to old build script

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-g++ -O2 main.cpp SDLBufferRenderer.cpp SDLInputChecker.cpp gb/gbz80cpu.cpp gb/gbmem.cpp gb/gbcart.cpp gb/gbpad.cpp gb/gblcd.cpp gb/gbserial.cpp -lSDL2 -o yagbe 
+g++ -O2 main.cpp SDLBufferRenderer.cpp SDLInputChecker.cpp gb/gbz80cpu.cpp gb/gbmem.cpp gb/gbcart.cpp gb/gbpad.cpp gb/gblcd.cpp gb/gbserial.cpp -lSDL2 -lSDL2_mixer -o yagbe 
