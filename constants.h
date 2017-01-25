d4faf9b5921894d75409978f5eaf766aad97069a
Elliot Hawkins <elliotshawkins@gmail.com>
2017-01-24 18:57:55 -0800

    update constants.h

--- constants.h
+++ constants.h
@@ -2,11 +2,13 @@
 
 #define CONSOLE_OUTPUT_ENABLED false
 #define CONSOLE_OUTPUT_REGISTERS false
-#define CONSOLE_OUTPUT_IO false
+#define CONSOLE_OUTPUT_IO true
+#define CONSOLE_OUTPUT_CART true
 #define STOP_ON_BAD_OPCODE true
 #define STOP_ON_BAD_STOP false
 #define STOP_ON_STOP false
 #define STOP_ON_HALT false
+#define IGNORE_HALT false
 #define BAD_STOP_IS_LCD_ON true
 #define SERIAL_ENABLED false
 #define BLAARG_TEST_OUTPUT false
